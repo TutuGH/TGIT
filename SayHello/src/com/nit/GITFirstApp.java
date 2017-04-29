71f287d58c0e6d40ac798494945b04b1e31a9e07
TuTuRaJ <TuTuRaJ@192.168.0.5>
2017-04-29 15:22:36 +0530

    Changes don by Dev 1

--- SayHello/src/com/nit/GITFirstApp.java
+++ SayHello/src/com/nit/GITFirstApp.java
@@ -3,7 +3,8 @@ package com.nit;
 public class GITFirstApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to GIT Firt App1");
+		System.out.println("Welcome to GIT Firt App");
+		System.out.println("Changes done by Developer 1");
 
 	}
 
